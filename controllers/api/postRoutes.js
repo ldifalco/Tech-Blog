9aa4db1c11a2e5314b7520819367d94edb8a778e
Lucas DiFalco <luke.difalco52@gmail.com>
2021-09-22 16:48:39 -0400

    more console logs

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,13 +3,17 @@ const { Post } = require('../../models/');
 const auth = require('../../utils/auth');
 
 router.post('/', auth, async (req, res) => {
+  console.log("it worked!")
     const body = req.body;
   
     try {
+    
       const newPost = await Post.create({ ...body, userId: req.session.userId });
       res.json(newPost);
     } catch (err) {
       res.status(500).json(err);
+      console.log("booo you stink")
+      
     }
   });
   
